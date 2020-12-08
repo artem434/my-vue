7246b115a0b9b6a83b2c87407fda25632466306a
7eminar <artem4342232@gmail.com>
2020-12-08 17:40:33 +0200

    deploy

---
+++ precache-manifest.c9c287371ba175e1c0effa5105114212.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adabaf077dcca5b37aa1",
+    "url": "/my-vue/css/app.0e433876.css"
+  },
+  {
+    "revision": "5db3895e13459c234899",
+    "url": "/my-vue/css/chunk-9c527828.e180436f.css"
+  },
+  {
+    "revision": "924b260a68bbe616ca2a",
+    "url": "/my-vue/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "4d9c4129fcd728729af90850c02409a9",
+    "url": "/my-vue/index.html"
+  },
+  {
+    "revision": "adabaf077dcca5b37aa1",
+    "url": "/my-vue/js/app.8687edf1.js"
+  },
+  {
+    "revision": "da77a6918addd23523dd",
+    "url": "/my-vue/js/chunk-2d0b64c8.1aba8288.js"
+  },
+  {
+    "revision": "5db3895e13459c234899",
+    "url": "/my-vue/js/chunk-9c527828.85899b07.js"
+  },
+  {
+    "revision": "924b260a68bbe616ca2a",
+    "url": "/my-vue/js/chunk-vendors.e64e9c07.js"
+  },
+  {
+    "revision": "555cd7e93bce8711aeaba4af24e86d3f",
+    "url": "/my-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-vue/robots.txt"
+  },
+  {
+    "revision": "00dd66464abf918f86ce134e571be283",
+    "url": "/my-vue/style.css"
+  }
+]);
